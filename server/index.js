205231fe7dc81cf4f168e3dfe03d66a52b08d43b
IonescuA <alina.ionescu@haufe-lexware.com>
2019-06-01 18:20:31 +0300

    Change server port to 8080

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const config = require('./config/config');
 const init = async () => {
 
     const server = Hapi.server({
-        port: 3000,
+        port: 8080,
         host: '0.0.0.0'
     });
 
